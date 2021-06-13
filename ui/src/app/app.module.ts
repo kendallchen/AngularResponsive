596bb393f1ef1a7f41222b78343c848438772873
support <support@dotnetlead.com>
2021-06-13 10:19:28 -0700

    add home component

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MaterialModule } from './common/material.module';
 
@@ -9,8 +9,12 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 import { CustomerComponent } from './customer/customer.component';
 import { CustomerDialog } from './customer/dialog/customer-dialog.component';
+
+const routes: Routes = [{ path: '', component: HomeComponent },
+                        { path: 'customer', component: CustomerComponent}];
 
 @NgModule({
   imports: [
@@ -20,16 +24,15 @@ import { CustomerDialog } from './customer/dialog/customer-dialog.component';
     HttpClientModule,
     BrowserAnimationsModule,
     MaterialModule,
-    RouterModule.forRoot([
-    { path: '', component: CustomerComponent },
-], { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes),
   ],
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
     CustomerComponent,
-    CustomerDialog
+    CustomerDialog,
+    HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
